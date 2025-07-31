aa3a3b6334dfdfc146271234fdf85a4f26478913
divyalashmi <kaviyakaviya31732@gmail.com>
2025-07-31 15:48:52 +0530

    Create def.py

---
+++ def.py
@@ -0,0 +1,6 @@
+def greet (things):
+    print('buy',things)
+things_list = ['egg','milk','sugar','corn flour']
+for things in things_list:
+    greet(things)def greet (things):
+    print('buy',things)
